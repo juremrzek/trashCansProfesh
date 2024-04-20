b35cee048117c692278f3aaf56d841c8a01db444
Jan Merhar <merharjan@gmail.com>
2024-04-20 21:19:26 +0200

    Update TabProfile.vue

--- client/src/views/TabProfile.vue
+++ client/src/views/TabProfile.vue
@@ -1,14 +1,32 @@
 <template>
   <tab-template>
     <template #header>
-      <tab-header>header </tab-header>
+      <tab-header>Statistike </tab-header>
     </template>
     <template #body>
-      <TransitionGroup name="list" tag="ul">
-        <li>banan</li>
-      </TransitionGroup>
-
-      <tab-no-elements :elements="[]"></tab-no-elements>
+      <list-container>
+        <template #title>
+          <list-title> Statistike </list-title>
+        </template>
+        <template #default>
+          <list-item>
+            <template #title> en astistika </template>
+            <template #value> banana party </template>
+          </list-item>
+          <list-item>
+            <template #title> en astistika </template>
+            <template #value> banana party </template>
+          </list-item>
+          <list-item>
+            <template #title> en astistika </template>
+            <template #value> banana party </template>
+          </list-item>
+          <list-item>
+            <template #title> en astistika </template>
+            <template #value> banana party </template>
+          </list-item>
+        </template>
+      </list-container>
     </template>
   </tab-template>
 </template>
@@ -16,5 +34,8 @@
 <script setup lang="ts">
 import TabTemplate from "@/components/ui-components/tab/TabTemplate.vue"
 import TabHeader from "@/components/ui-components/tab/TabHeader.vue"
-import TabNoElements from "@/components/ui-components/tab/TabNoElements.vue"
+
+import ListContainer from "@/components/ui-components/list/ListContainer.vue"
+import ListTitle from "@/components/ui-components/list/ListTitle.vue"
+import ListItem from "@/components/ui-components/list/ListItem.vue"
 </script>
