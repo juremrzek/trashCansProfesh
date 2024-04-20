daa982617e255e993b1a4ddeb0566216e017616e
Jure <jure.mrzek15@gmail.com>
2024-04-20 16:01:34 +0200

    Add game logic to static pages

--- static/javascript/main.js
+++ static/javascript/main.js
@@ -1,4 +1,28 @@
-const canvas = document.getElementById("canvas");
-canvas.width = 1200;
-canvas.height = 800;
+//example class usage
 
+function generateTrash() {
+    random_category = Trash.category_options[Math.floor(Math.random() * Trash.category_options.length)];
+    return new Trash(category);
+}
+
+const player = new Player("Professional gamer");
+
+// current trash ki je bil throwan
+const current_trash = new Trash("paper");
+
+//can katerega je zadel trash
+const current_can = new Can("paper");
+
+if (current_trash.category == current_can.category) {
+    player.score += 1;
+    current_trash = generateTrash()
+    if (player.score >= required_score) {
+        // level complete screen
+    }
+}
+else {
+    if (player.score > player.record){
+        player.record = player.score;
+    }
+    // game over screen
+}
