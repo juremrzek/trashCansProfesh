086c5cc697c9e5a93718deb93004ec48315abd03
Jan Merhar <merharjan@gmail.com>
2024-04-20 19:50:21 +0200

    Update TabHeader.vue

--- client/src/components/ui-components/tab/TabHeader.vue
+++ client/src/components/ui-components/tab/TabHeader.vue
@@ -2,17 +2,30 @@
   <h3 class="ion-text-center">
     <slot></slot>
   </h3>
+  <div class="line"></div>
 </template>
 
 <style scoped>
 h3 {
   text-transform: uppercase;
-  font-size: 20px;
+  font-size: 6vh;
   letter-spacing: 1px;
   font-weight: 500;
   margin: 0;
-  padding: 5px;
+  padding-bottom: 5px;
+  padding-top: 5px;
+  height: 8vh;
+  vertical-align: middle;
   background-color: var(--ion-color-primary);
-  color: var(--ion-color-light);
+  color: var(--ion-color-tertiary);
+}
+</style>
+
+<style>
+.line {
+  height: 2px;
+  background: black;
+  width: 100vw;
+  border-bottom: 5px solid var(--ion-color-tertiary);
 }
 </style>
