e249c3ee315970036dde82015d3f3bf1345f4bac
Jan Merhar <merharjan@gmail.com>
2024-04-20 23:51:59 +0200

    Add TabInfo.vue

---
+++ client/src/views/TabInfo.vue
@@ -0,0 +1,14 @@
+<template>
+  <tab-template>
+    <template #header>
+      <tab-header>header </tab-header>
+    </template>
+    <template #body> koda koda koda koda koda koda </template>
+  </tab-template>
+</template>
+
+<script setup lang="ts">
+import TabTemplate from "@/components/ui-components/tab/TabTemplate.vue"
+import TabHeader from "@/components/ui-components/tab/TabHeader.vue"
+import TabNoElements from "@/components/ui-components/tab/TabNoElements.vue"
+</script>
