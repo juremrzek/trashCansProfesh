31bc47991790eb0bb56c0fc2b80a9e2e195f9bf9
Jan Merhar <merharjan@gmail.com>
2024-04-21 10:34:40 +0200

    Update TrashBox.vue

--- client/src/components/ui-components/gameElement/TrashBox.vue
+++ client/src/components/ui-components/gameElement/TrashBox.vue
@@ -1,34 +1,41 @@
 <template>
-    <div class="trash-box-border">
-        <div class="trash-box">
-            <img src="../../../../public/assets/trashImages/embalaza/vrecka.png" />
-        </div>
+  <div class="trash-box-border">
+    <div class="trash-box">
+      <img :src="url" />
     </div>
-  </template>
-  
+  </div>
+</template>
 
-  <style scoped>
-  .trash-box {
-    width: clamp(80px, 8vw, 8vw);
-    height: clamp(80px, 8vw, 8vw);
-    background: linear-gradient(var(--ion-color-secondary), var(--ion-color-secondary-dark-shade));
-    border-radius: 10px;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-  }
-  .trash-box-border {
-    width: clamp(90px, 9vw, 9vw);
-    height: clamp(90px, 9vw, 9vw);
-    background:  linear-gradient(var(--ion-color-danger-contrast), #A3E5FF);
-    border-radius: 10px;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-  }
-  img {
-    width: 50%;
-    object-fit: cover;
-  }
-  </style>
-  
+<script setup lang="ts">
+defineProps({
+  url: String,
+})
+</script>
+
+<style scoped>
+.trash-box {
+  width: clamp(80px, 8vw, 8vw);
+  height: clamp(80px, 8vw, 8vw);
+  background: linear-gradient(
+    var(--ion-color-secondary),
+    var(--ion-color-secondary-dark-shade)
+  );
+  border-radius: 10px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+.trash-box-border {
+  width: clamp(90px, 9vw, 9vw);
+  height: clamp(90px, 9vw, 9vw);
+  background: linear-gradient(var(--ion-color-danger-contrast), #a3e5ff);
+  border-radius: 10px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+img {
+  width: 50%;
+  object-fit: cover;
+}
+</style>
