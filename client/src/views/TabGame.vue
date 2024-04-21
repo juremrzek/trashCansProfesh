16f0199376ef974d2ddc11631fb2964ca5c8adf8
Jan Merhar <merharjan@gmail.com>
2024-04-21 10:26:44 +0200

    Update TabGame.vue

--- client/src/views/TabGame.vue
+++ client/src/views/TabGame.vue
@@ -18,10 +18,9 @@
         </template>
       </top-controls>
       <backgroud-image>
-        <trash-box></trash-box>
-        <trash-box></trash-box>
-        <trash-box></trash-box>
-        <trash-box></trash-box>
+        <trash-footer>
+          <trash-box></trash-box>
+        </trash-footer>
       </backgroud-image>
     </template>
   </tab-template>
@@ -33,6 +32,7 @@ import TrashBox from "@/components/ui-components/gameElement/TrashBox.vue"
 import TopControls from "@/components/ui-components/tab/TopControls.vue"
 import BoxInfo from "@/components/ui-components/gameElement/BoxInfo.vue"
 import BackgroudImage from "@/components/ui-components/gameElement/BackgroundImage.vue"
+import TrashFooter from "@/components/ui-components/TrashFooter.vue"
 import { useRouter } from "vue-router"
 import { usePlayerStore } from "@/stores/usePlayerStore"
 import { storeToRefs } from "pinia"
