11ccd808edfe8ef530c723beb98aa4a3592fb40a
Jan Merhar <merharjan@gmail.com>
2024-04-21 09:54:19 +0200

    Update TrashFooter.vue

--- client/src/components/ui-components/TrashFooter.vue
+++ client/src/components/ui-components/TrashFooter.vue
@@ -1,26 +1,26 @@
 <template>
-      <div class="trash-box">
-        <slot></slot>
-      </div>
-  </template>
-  
-  <script setup lang="ts">
-    import TrashBox from "@/components/ui-components/gameElement/TrashBox.vue";
-</script>
+  <div class="trash-box">
+    <slot></slot>
+  </div>
+</template>
 
-  <style scoped>
-  .trash-box {
-    width: 100%;
-    height: 20vh;
-    background: var(--ion-color-primary); 
-    border: 10px solid var(--ion-color-tertiary);
-    display: flex;
-    align-items: center;
-    justify-content: center;
-  }
-  img {
-    width: 50%;
-    object-fit: cover;
-  }
-  </style>
-  
+<style scoped>
+.trash-box {
+  width: 100%;
+  height: 20vh;
+  border: 10px solid var(--ion-color-tertiary);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 150px;
+  position: fixed;
+  bottom: 0%;
+  width: 100%;
+  background-color: var(--ion-color-primary);
+  opacity: 1;
+}
+img {
+  width: 50%;
+  object-fit: cover;
+}
+</style>
