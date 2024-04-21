b9d015a9fbf8a8bfc353a54e8e14f70eca85a673
Jan Merhar <merharjan@gmail.com>
2024-04-21 08:48:27 +0200

    Finish tabProfile.vue

--- client/src/views/TabProfile.vue
+++ client/src/views/TabProfile.vue
@@ -9,21 +9,30 @@
           <list-title> Statistike </list-title>
         </template>
         <template #default>
+          <!-- Score -->
           <list-item>
-            <template #title> en astistika </template>
-            <template #value> banana party </template>
+            <template #title>Seštevek točk</template>
+            <template #value>{{ score }}</template>
           </list-item>
+          <!-- Fails -->
           <list-item>
-            <template #title> en astistika </template>
-            <template #value> banana party </template>
+            <template #title>Spodrslaji</template>
+            <template #value>{{ fails.length }}</template>
           </list-item>
+          <!-- Streak -->
           <list-item>
-            <template #title> en astistika </template>
-            <template #value> banana party </template>
+            <template #title>Niz pravilnih rešitev</template>
+            <template #value>{{ streak }}</template>
           </list-item>
+          <!-- Max streak -->
           <list-item>
-            <template #title> en astistika </template>
-            <template #value> banana party </template>
+            <template #title>Najdaljši niz pravilnih rešitev</template>
+            <template #value>{{ maxStreak }}</template>
+          </list-item>
+          <!-- Max level -->
+          <list-item>
+            <template #title>Doseženi level</template>
+            <template #value>{{ maxLevel }}</template>
           </list-item>
         </template>
       </list-container>
@@ -38,4 +47,11 @@ import TabHeader from "@/components/ui-components/tab/TabHeader.vue"
 import ListContainer from "@/components/ui-components/list/ListContainer.vue"
 import ListTitle from "@/components/ui-components/list/ListTitle.vue"
 import ListItem from "@/components/ui-components/list/ListItem.vue"
+
+import { usePlayerStore } from "@/stores/usePlayerStore"
+import { storeToRefs } from "pinia"
+
+const { score, fails, streak, maxStreak, maxLevel } = storeToRefs(
+  usePlayerStore()
+)
 </script>
